e7494fe5c8f606f008f57799ebcec380bfbe9311
Vladimir Kosykh <vskos@mail.ru>
2022-02-01 10:46:37 +0600

    locators added

--- test_main_page.py
+++ test_main_page.py
@@ -2,6 +2,7 @@ from .pages.main_page import MainPage
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
+
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
